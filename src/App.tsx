6d70f6cae915251865753798507961b3c215114f
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-17 15:07:28 +0200

    create a simple react info site

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} alt="react logo" />
+        <h1>
+          Fun facts about React:
+        </h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is mainitained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
