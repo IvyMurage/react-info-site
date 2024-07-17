86541320e68ceff7165064e28c43963f25e60be5
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-17 17:02:46 +0200

    feat: Create Nav and Main component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
-      <div>
-        <img src={reactLogo} alt="react logo" />
-        <h1>
-          Fun facts about React:
-        </h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on Github</li>
-          <li>Is mainitained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </div>
+      <Navbar />
+      <Main />
     </>
   )
 }
